af967bd41de5be9e9722d6c03c7986450ae0d2e0
Mesqualito <jochen@gebsattel.rocks>
2019-03-15 23:30:29 +0100

    Analog zu 'https://www.udemy.com/spring-framework-5-beginner-to-guru/learn/v4/t/lecture/11247966'
    Erste Versuche der WRO4J-Integration mit 'https://github.com/wro4j/wro4j-gradle-plugin' (letzter Commit bei dessen GitHub-Sourcen am 4.08.2016!), nach Anleitung unter 'https://github.com/IlyaAI/wro4j-gradle-plugin' bzw. 'https://github.com/wro4j/wro4j-boot' (letzter Commit dort ist noch älter, vom 16.09.2015).
    
    Laufen tut es in diesem Commit-Stand nicht, es werden keine css-files aus den less-Definitionen erzeugt und demzufolge auch nicht angezeigt. Die Anwendung selbst lässt sich kompilieren und ausführen.
    
    Im nächsten Schritt werde ich die Anleitung unter 'https://wro4j.readthedocs.io/en/stable/ConfigureWro4jViaSpring/' versuchen...
    
    Dieser Commit dient zur Archivierung, Historisierung und zur Dokumentation.
    
    Aufgrund von Versions-, Modul- und Technikänderungen müssen die Stunden der Recherchen, Tests und Analysen von unvorhergesehenen Problemen selbst bei 'curated solutions' rund um das Spring framework und gradle herum immer in die Vorüberlegungen zu einem Projekt fest einbezogen werden!
    
    Relates to #23

--- fetenarchiv-web/src/main/java/com/eigenbaumarkt/fetenarchiv/FetenarchivApplication.java
+++ fetenarchiv-web/src/main/java/com/eigenbaumarkt/fetenarchiv/FetenarchivApplication.java
@@ -1,12 +1,10 @@
 package com.eigenbaumarkt.fetenarchiv;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class FetenarchivApplication {
 
     public static void main(String[] args) {
