ce28f88c4e807a8f27f37644f642b00666dadccb
Mesqualito <jochen@gebsattel.rocks>
2019-04-16 10:26:44 +0200

    Analog zu 'https://www.udemy.com/spring-framework-5-beginner-to-guru/learn/v4/t/lecture/11309006' (Abschnitt 8, Lektion 152)
    Closes #35

--- fetenarchiv-daten/src/main/java/com/eigenbaumarkt/fetenarchiv/model/Basis.java
+++ fetenarchiv-daten/src/main/java/com/eigenbaumarkt/fetenarchiv/model/Basis.java
@@ -1,11 +1,21 @@
 package com.eigenbaumarkt.fetenarchiv.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+// die Annotation teilt Spring Data JPA mit,
+// dass die Klasse vererbt wird und nicht
+// selbst gespeichert zu werden braucht
+@MappedSuperclass
 public class Basis implements Serializable {
 
     // Recommendation of Hibernate:
     // use Box-Types (not 'long', but 'Long')
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
