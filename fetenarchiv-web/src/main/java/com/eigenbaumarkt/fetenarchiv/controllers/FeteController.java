2a0a7f266783c695880bb2f7fafa58336a1440fd
Jochen <jo@hassfurter.com>
2020-08-26 17:01:54 +0200

    BUGGY - in der Navigation "Mediafiles" vs. "Veterinarians" ? Zudem unter "http://localhost:8080/feten" keine Objekte. Überprüfen!
    analog zu 'https://www.udemy.com/course/spring-framework-5-beginner-to-guru/learn/lecture/12663748' - Lektion 12/244

--- fetenarchiv-web/src/main/java/com/eigenbaumarkt/fetenarchiv/controllers/FeteController.java
+++ fetenarchiv-web/src/main/java/com/eigenbaumarkt/fetenarchiv/controllers/FeteController.java
@@ -1,4 +1,25 @@
 package com.eigenbaumarkt.fetenarchiv.controllers;
 
+import com.eigenbaumarkt.fetenarchiv.services.FeteService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class FeteController {
+
+    private final FeteService feteService;
+
+    public FeteController(FeteService feteService) {
+        this.feteService = feteService;
+    }
+
+
+    @RequestMapping({"/feten", "/feten/index", "/feten/index.html", "/feten.html"})
+    public String listFeten(Model model) {
+
+        model.addAttribute("feten", feteService.findAll());
+
+        return "feten/index";
+    }
 }
