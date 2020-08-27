a85c1df087f018fb752811f2d61872a5f3782ae5
Jochen <jo@hassfurter.com>
2020-08-27 12:58:03 +0200

    "Feten" JSON Endpoint (API) hinzu gefügt, closes #70
    analog zu 'https://www.udemy.com/course/spring-framework-5-beginner-to-guru/learn/lecture/12663752' - Lektion 12/245

--- fetenarchiv-web/src/main/java/com/eigenbaumarkt/fetenarchiv/controllers/FeteController.java
+++ fetenarchiv-web/src/main/java/com/eigenbaumarkt/fetenarchiv/controllers/FeteController.java
@@ -1,9 +1,14 @@
 package com.eigenbaumarkt.fetenarchiv.controllers;
 
+import com.eigenbaumarkt.fetenarchiv.model.Fete;
 import com.eigenbaumarkt.fetenarchiv.services.FeteService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class FeteController {
@@ -15,11 +20,20 @@ public class FeteController {
     }
 
 
-    @RequestMapping({"/feten", "/feten/index", "/feten/index.html", "/feten.html"})
+    @GetMapping({"/feten", "/feten/index", "/feten/index.html", "/feten.html"})
     public String listFeten(Model model) {
 
         model.addAttribute("feten", feteService.findAll());
 
         return "feten/index";
     }
+
+    // @ResponseBody liefert entweder JSON (default) oder XML (muss konfiguriert werden)
+    @RequestMapping("/api/feten")
+    public @ResponseBody Set<Fete> getFetenJson() {
+
+        return feteService.findAll();
+
+    }
+
 }
